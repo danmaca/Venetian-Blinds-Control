74a721f81cf3befca0ff944b8eb4d4320a3cd989
Daniel Macek <danmaca@centrum.cz>
2023-01-14 23:59:38 +0100

    reformat * type and use tab spacing

--- components/venetian_blinds/venetian_blinds.cpp
+++ components/venetian_blinds/venetian_blinds.cpp
@@ -3,152 +3,153 @@
 #include "esphome/core/hal.h"
 
 namespace esphome {
-namespace venetian_blinds {
+	namespace venetian_blinds {
 
-static const char *TAG = "venetian_blinds.cover";
-static const int WaitTimeDefault = 180;// waiting for movement start, when little tilt change
+		static const char* TAG = "venetian_blinds.cover";
+		static const int WaitTimeDefault = 180;// waiting for movement start, when little tilt change
 
-using namespace esphome::cover;
+		using namespace esphome::cover;
 
-void VenetianBlinds::dump_config() {
-    LOG_COVER("", "Venetian Blinds", this);
-    ESP_LOGCONFIG(TAG, "  Open Duration: %.1fs", this->open_duration / 1e3f);
-    ESP_LOGCONFIG(TAG, "  Close Duration: %.1fs", this->close_duration / 1e3f);
-    ESP_LOGCONFIG(TAG, "  Tilt Duration: %.1fs", this->tilt_duration / 1e3f);
-}
+		void VenetianBlinds::dump_config() {
+			LOG_COVER("", "Venetian Blinds", this);
+			ESP_LOGCONFIG(TAG, "  Open Duration: %.1fs", this->open_duration / 1e3f);
+			ESP_LOGCONFIG(TAG, "  Close Duration: %.1fs", this->close_duration / 1e3f);
+			ESP_LOGCONFIG(TAG, "  Tilt Duration: %.1fs", this->tilt_duration / 1e3f);
+		}
 
-void VenetianBlinds::setup() {   
-    auto restore = this->restore_state_();
-    if (restore.has_value()) {
-        restore->apply(this);
-    } else {
-        this->position = 0.0;
-        this->tilt = 0.0;
-    }
+		void VenetianBlinds::setup() {
+			auto restore = this->restore_state_();
+			if (restore.has_value()) {
+				restore->apply(this);
+			}
+			else {
+				this->position = 0.0;
+				this->tilt = 0.0;
+			}
 
-    exact_pos = this->position * this->close_duration;
-    exact_tilt = this->tilt * this->tilt_duration;
+			exact_pos = this->position * this->close_duration;
+			exact_tilt = this->tilt * this->tilt_duration;
 
-    ESP_LOGCONFIG(TAG, "Initial position: %.1f", this->position * 100);
-    ESP_LOGCONFIG(TAG, "Initial tilt: %.1f", this->tilt * 100);
-}
+			ESP_LOGCONFIG(TAG, "Initial position: %.1f", this->position * 100);
+			ESP_LOGCONFIG(TAG, "Initial tilt: %.1f", this->tilt * 100);
+		}
 
-CoverTraits VenetianBlinds::get_traits() {
-    auto traits = CoverTraits();
-    traits.set_supports_position(true);
-    traits.set_supports_tilt(true);
-    traits.set_is_assumed_state(this->assumed_state);
-    return traits;
-}
+		CoverTraits VenetianBlinds::get_traits() {
+			auto traits = CoverTraits();
+			traits.set_supports_position(true);
+			traits.set_supports_tilt(true);
+			traits.set_is_assumed_state(this->assumed_state);
+			return traits;
+		}
 
-void VenetianBlinds::control(const CoverCall &call) {
-    if (call.get_position().has_value()) {
-        starting_pos = exact_pos;
-        starting_tilt = exact_tilt;
-        int new_pos = *call.get_position() * this->close_duration;
-        change_pos = exact_pos - new_pos;
-        rest_pos = change_pos;
-        change_tilt = 0;
-        rest_tilt = 0;
-    }
+		void VenetianBlinds::control(const CoverCall& call) {
+			if (call.get_position().has_value()) {
+				starting_pos = exact_pos;
+				starting_tilt = exact_tilt;
+				int new_pos = *call.get_position() * this->close_duration;
+				change_pos = exact_pos - new_pos;
+				rest_pos = change_pos;
+				change_tilt = 0;
+				rest_tilt = 0;
+			}
 
-    if (call.get_tilt().has_value()) {
-        starting_pos = exact_pos;
-        starting_tilt = exact_tilt;
-        int new_tilt = *call.get_tilt() * this->tilt_duration;
-        change_tilt = exact_tilt - new_tilt;
-        rest_tilt = change_tilt;
-        change_pos = 0;
-        rest_pos = 0;
-    }
+			if (call.get_tilt().has_value()) {
+				starting_pos = exact_pos;
+				starting_tilt = exact_tilt;
+				int new_tilt = *call.get_tilt() * this->tilt_duration;
+				change_tilt = exact_tilt - new_tilt;
+				rest_tilt = change_tilt;
+				change_pos = 0;
+				rest_pos = 0;
+			}
 
-    if (call.get_position().has_value() || call.get_tilt().has_value()) {
-        starting_time = millis();
-        publishingDelay = 0;
-    }
+			if (call.get_position().has_value() || call.get_tilt().has_value()) {
+				starting_time = millis();
+				publishingDelay = 0;
+			}
 
-    if (call.get_stop()) {
-        rest_pos = 0;
-        rest_tilt = 0;
-        change_pos = 0;
-        change_tilt = 0;
-        this->stop_trigger->trigger();
-        this->current_action = COVER_OPERATION_IDLE;
-        this->publishCoverState();
-    }
-}
+			if (call.get_stop()) {
+				rest_pos = 0;
+				rest_tilt = 0;
+				change_pos = 0;
+				change_tilt = 0;
+				this->stop_trigger->trigger();
+				this->current_action = COVER_OPERATION_IDLE;
+				this->publishCoverState();
+			}
+		}
 
-void VenetianBlinds::loop() {
-    if (wait_time > 0) {
-        if (wait_time > (millis() - starting_time))
-            return;
-        else {
-            wait_time = 0;
-            starting_time = millis();
-        }
-    }
+		void VenetianBlinds::loop() {
+			if (wait_time > 0) {
+				if (wait_time > (millis() - starting_time))
+					return;
+				else {
+					wait_time = 0;
+					starting_time = millis();
+				}
+			}
 
-    if (rest_pos > 0 || rest_tilt < 0) {
-        if (this->current_action != COVER_OPERATION_CLOSING) {
-            this->close_trigger->trigger();
-            this->current_action = COVER_OPERATION_CLOSING;
-            wait_time = WaitTimeDefault;
-            return;
-        }
+			if (rest_pos > 0 || rest_tilt < 0) {
+				if (this->current_action != COVER_OPERATION_CLOSING) {
+					this->close_trigger->trigger();
+					this->current_action = COVER_OPERATION_CLOSING;
+					wait_time = WaitTimeDefault;
+					return;
+				}
 
-        uint32_t current_time = millis();
-        int delta_time = current_time - starting_time;
-        publishingDelay++;
+				uint32_t current_time = millis();
+				int delta_time = current_time - starting_time;
+				publishingDelay++;
 
-        rest_tilt = clamp(change_tilt + delta_time, -1 * this->tilt_duration, 0);
-        exact_tilt = clamp(starting_tilt + delta_time, 0, this->tilt_duration);
+				rest_tilt = clamp(change_tilt + delta_time, -1 * this->tilt_duration, 0);
+				exact_tilt = clamp(starting_tilt + delta_time, 0, this->tilt_duration);
 
-        rest_pos = clamp(change_pos - delta_time, 0, this->close_duration);
-        exact_pos = clamp(starting_pos - delta_time, 0, this->close_duration);
+				rest_pos = clamp(change_pos - delta_time, 0, this->close_duration);
+				exact_pos = clamp(starting_pos - delta_time, 0, this->close_duration);
 
-        if (rest_pos <= 0 && rest_tilt >= 0) {
-            this->stop_trigger->trigger();
-            this->current_action = COVER_OPERATION_IDLE;
-            this->publishCoverState();
-        }
-        else if (publishingDelay % 100 == 0) {
-            this->publishCoverState();
-        }
-    } 
-    else if (rest_pos < 0 || rest_tilt > 0) {
-        if (this->current_action != COVER_OPERATION_OPENING) {
-            this->open_trigger->trigger();
-            this->current_action = COVER_OPERATION_OPENING;
-            wait_time = WaitTimeDefault;
-            return;
-        }
+				if (rest_pos <= 0 && rest_tilt >= 0) {
+					this->stop_trigger->trigger();
+					this->current_action = COVER_OPERATION_IDLE;
+					this->publishCoverState();
+				}
+				else if (publishingDelay % 100 == 0) {
+					this->publishCoverState();
+				}
+			}
+			else if (rest_pos < 0 || rest_tilt > 0) {
+				if (this->current_action != COVER_OPERATION_OPENING) {
+					this->open_trigger->trigger();
+					this->current_action = COVER_OPERATION_OPENING;
+					wait_time = WaitTimeDefault;
+					return;
+				}
 
-        uint32_t current_time = millis();
-        int delta_time = current_time - starting_time;
-        publishingDelay++;
+				uint32_t current_time = millis();
+				int delta_time = current_time - starting_time;
+				publishingDelay++;
 
-        rest_tilt = clamp(change_tilt - delta_time, 0, this->tilt_duration);
-        exact_tilt = clamp(starting_tilt - delta_time, 0, this->tilt_duration);
+				rest_tilt = clamp(change_tilt - delta_time, 0, this->tilt_duration);
+				exact_tilt = clamp(starting_tilt - delta_time, 0, this->tilt_duration);
 
-        rest_pos = clamp(change_pos + delta_time, -1 * this->close_duration, 0);
-        exact_pos = clamp(starting_pos + delta_time, 0, this->close_duration);
+				rest_pos = clamp(change_pos + delta_time, -1 * this->close_duration, 0);
+				exact_pos = clamp(starting_pos + delta_time, 0, this->close_duration);
 
-        if (rest_pos >= 0 && rest_tilt <= 0) {
-            this->stop_trigger->trigger();
-            this->current_action = COVER_OPERATION_IDLE;
-            this->publishCoverState();
-        }
-        else if (publishingDelay % 100 == 0) {
-            this->publishCoverState();
-        }
-    }
-}
+				if (rest_pos >= 0 && rest_tilt <= 0) {
+					this->stop_trigger->trigger();
+					this->current_action = COVER_OPERATION_IDLE;
+					this->publishCoverState();
+				}
+				else if (publishingDelay % 100 == 0) {
+					this->publishCoverState();
+				}
+			}
+		}
 
-void VenetianBlinds::publishCoverState() {
-    this->position = exact_pos / (float)this->close_duration;
-    this->tilt = exact_tilt / (float)this->tilt_duration;
-    this->publish_state();
-};
+		void VenetianBlinds::publishCoverState() {
+			this->position = exact_pos / (float)this->close_duration;
+			this->tilt = exact_tilt / (float)this->tilt_duration;
+			this->publish_state();
+		};
 
-}
+	}
 }
--- components/venetian_blinds/venetian_blinds.h
+++ components/venetian_blinds/venetian_blinds.h
@@ -4,45 +4,45 @@
 #include "esphome/components/cover/cover.h"
 
 namespace esphome {
-namespace venetian_blinds {
+	namespace venetian_blinds {
 
-class VenetianBlinds : public Component, public cover::Cover {
-  public:
-    void setup() override;
-    void loop() override;
-    void dump_config() override;
-    cover::CoverTraits get_traits() override;
-    void control(const cover::CoverCall &call) override;
-    Trigger<> *get_open_trigger() const { return this->open_trigger; }
-    Trigger<> *get_close_trigger() const { return this->close_trigger; }
-    Trigger<> *get_stop_trigger() const { return this->stop_trigger; }    
-    void set_open_duration(int open) { this->open_duration = open; }
-    void set_close_duration(int close) { this->close_duration = close; }
-    void set_tilt_duration(int tilt) { this->tilt_duration = tilt; }
-    void set_assumed_state(bool value) { this->assumed_state = value; }
-  private:
-    int starting_time{0};
-    int wait_time{0};
-    int exact_pos{0};
-    int starting_pos{0};
-    int rest_pos{0};
-    int change_pos{0};
-    int exact_tilt{0};
-    int starting_tilt{0};
-    int rest_tilt{0};
-    int change_tilt{0};
-    int publishingDelay{0};
-    cover::CoverOperation current_action{cover::COVER_OPERATION_IDLE};
-    void publishCoverState();
-  protected:
-    Trigger<> *open_trigger{new Trigger<>()};
-    Trigger<> *close_trigger{new Trigger<>()};
-    Trigger<> *stop_trigger{new Trigger<>()};
-    int open_duration;
-    int close_duration;
-    int tilt_duration;
-    bool assumed_state{false};
-};
+		class VenetianBlinds : public Component, public cover::Cover {
+		public:
+			void setup() override;
+			void loop() override;
+			void dump_config() override;
+			cover::CoverTraits get_traits() override;
+			void control(const cover::CoverCall& call) override;
+			Trigger<>* get_open_trigger() const { return this->open_trigger; }
+			Trigger<>* get_close_trigger() const { return this->close_trigger; }
+			Trigger<>* get_stop_trigger() const { return this->stop_trigger; }
+			void set_open_duration(int open) { this->open_duration = open; }
+			void set_close_duration(int close) { this->close_duration = close; }
+			void set_tilt_duration(int tilt) { this->tilt_duration = tilt; }
+			void set_assumed_state(bool value) { this->assumed_state = value; }
+		private:
+			int starting_time{ 0 };
+			int wait_time{ 0 };
+			int exact_pos{ 0 };
+			int starting_pos{ 0 };
+			int rest_pos{ 0 };
+			int change_pos{ 0 };
+			int exact_tilt{ 0 };
+			int starting_tilt{ 0 };
+			int rest_tilt{ 0 };
+			int change_tilt{ 0 };
+			int publishingDelay{ 0 };
+			cover::CoverOperation current_action{ cover::COVER_OPERATION_IDLE };
+			void publishCoverState();
+		protected:
+			Trigger<>* open_trigger{ new Trigger<>() };
+			Trigger<>* close_trigger{ new Trigger<>() };
+			Trigger<>* stop_trigger{ new Trigger<>() };
+			int open_duration;
+			int close_duration;
+			int tilt_duration;
+			bool assumed_state{ false };
+		};
 
-}
+	}
 }
